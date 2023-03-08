af6e4ff7f85eddb84dc8685f663ff40e0b9a40ab
luisoh93 <luis_oh@hotmail.com>
2023-03-08 04:17:27 -0800

    hotfix

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
- * _strlen_recursion - Returns the length of a string
+ * _strlen_recursion = returns the lenght of a string
+ *
  * @s: The string to be measured
  *
  * return: the length of the string.
