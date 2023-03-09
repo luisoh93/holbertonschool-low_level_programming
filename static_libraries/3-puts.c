f34f95bc6fa59bc8e3779d2069f541e927fd1858
luisoh93 <luis_oh@hotmail.com>
2023-03-09 04:07:55 -0800

    static libraries

---
+++ static_libraries/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include<stdio.h>
+
+/**
+ * _puts -fuction that prints a string, followed by a new line, to stdout
+ *
+ * @str: parameter defined in main
+ *
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+}
